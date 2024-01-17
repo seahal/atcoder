1ac9b5d8fd9f4344886ca36140a43118c62fed51
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-01-18 03:24:26 +0900

    Create pocp-a2-2.rs

---
+++ src/pocp-a2-2.rs
@@ -0,0 +1,23 @@
+// 競技プログラミングの鉄則(ISBN: 978-4-8399-7750-4)
+// A02, Linear Search
+
+fn main() { 
+    let mut s = String::new();
+    let _ = std::io::stdin().read_line(&mut s);
+    let fst = s
+        .trim()
+        .split_whitespace()
+        .flat_map(|o| o.parse::<isize>())
+        .nth(1)
+        .unwrap();
+
+    s.clear();
+    let _ = std::io::stdin().read_line(&mut s);
+    let result = s
+        .trim()
+        .split_whitespace()
+        .flat_map(|o| o.parse::<isize>())
+        .any(|v| v == fst);
+
+    println!("{}", if result { "Yes" } else { "No" });
+}
