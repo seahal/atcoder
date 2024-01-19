948149e4d72565e0ac94d3eb21585a7b19da0401
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-01-19 22:45:22 +0900

    i solved the quiestion.

--- src/pocp-a11.rb
+++ src/pocp-a11.rb
@@ -1,20 +1,19 @@
 # 競技プログラミングの鉄則(ISBN: 978-4-8399-7750-4)
-# A11 - Binary Search 1, TLE
+# A11 - Binary Search 1
 
-n, x = gets.split(" ").map(&:to_i)
+nr, x = gets.split(" ").map(&:to_i)
 a = gets.split(" ").map(&:to_i)
 
 left = 0
-right = n
 
-while left < right do
-  mean = (left + right) / 2
+while left <= nr do
+  mean = (left + nr) / 2
   if a[mean] < x
-    left = mean - 1
+    left = mean + 1
   elsif a[mean] == x
     puts mean + 1
     break;
   else
-    right = mean + 1
+    nr = mean - 1
   end
 end
