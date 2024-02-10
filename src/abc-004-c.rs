6b6bcbf73c07a9c8ab8c3a5cb690a1d85457fed2
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-02-10 17:03:29 +0900

    did atcode the day.

--- src/abc-004-c.rs
+++ src/abc-004-c.rs
@@ -4,12 +4,15 @@
 fn main() {
     let mut buffer = String::new();
     let _ = std::io::stdin().read_line(&mut buffer);
-    let n = buffer.trim().parse::<usize>().unwrap();
+    let n = buffer.trim().parse::<usize>().unwrap() % 30;
     let mut nums = vec![1, 2, 3, 4, 5, 6];
 
-    for i in 0..(n % 30) {
+    for i in 0..n {
         let j = i % 5 + 1;
-        nums.swap(j - 1, j);
+        let fst = nums[j - 1];
+        let snd = nums[j];
+        nums[j - 1] = snd;
+        nums[j] = fst;
     }
 
     for i in &nums {
